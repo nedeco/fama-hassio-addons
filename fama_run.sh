00616ac1dc79c1269994f330e9971c39e93f36e2
n.brendt <n.brendt@nedeco.de>
2025-02-05 22:59:56 +0100

    feat: added debug option and fixed config

--- fama_run.sh
+++ fama_run.sh
@@ -3,12 +3,13 @@
 export ENABLE_SENSOR_STATION=$(bashio::config "enable_sensor_station")
 export SMART_HOME_TYPE=$(bashio::config "smart_home_type")
 export HOME_ASSISTANT_DISCOVERY_PREFIX=$(bashio::config "home_assistant_discovery_prefix")
+export DEBUG=$(bashio::config "debug")
 
 export MQTT_HOST=$(bashio::services mqtt "host")
 export MQTT_PORT=$(bashio::services mqtt "port")
 export MQTT_TLS_ENABLED=$(bashio::services mqtt "ssl")
 export MQTT_USERNAME=$(bashio::services mqtt "username")
 export MQTT_PASSWORD=$(bashio::services mqtt "password")
-export MQTT_CLIENT_ID=$(bashio::config "smart_home_type")
+export MQTT_CLIENT_ID=$(bashio::config "mqtt_client_id")
 
 java -jar /fama.jar
