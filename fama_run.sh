fbda02ec5efaf839a68b66c00ace841eaece1897
n.brendt <n.brendt@nedeco.de>
2025-02-05 23:13:04 +0100

    feat: added mqtt log

--- fama_run.sh
+++ fama_run.sh
@@ -5,11 +5,19 @@ export SMART_HOME_TYPE=$(bashio::config "smart_home_type")
 export HOME_ASSISTANT_DISCOVERY_PREFIX=$(bashio::config "home_assistant_discovery_prefix")
 export DEBUG=$(bashio::config "debug")
 
-export MQTT_HOST=$(bashio::services mqtt "host")
-export MQTT_PORT=$(bashio::services mqtt "port")
-export MQTT_TLS_ENABLED=$(bashio::services mqtt "ssl")
+if ! bashio::services.available "mqtt"; then
+  bashio::log.error "No internal MQTT service found"
+else
+  bashio::log.info "MQTT service found, fetching credentials ..."
+  export MQTT_HOST=$(bashio::services mqtt "host")
+  export MQTT_PORT=$(bashio::services mqtt "port")
+  export MQTT_TLS_ENABLED=$(bashio::services mqtt "ssl")
+fi
+
 export MQTT_USERNAME=$(bashio::services mqtt "username")
 export MQTT_PASSWORD=$(bashio::services mqtt "password")
 export MQTT_CLIENT_ID=$(bashio::config "mqtt_client_id")
+
+env
 
 java -jar /fama.jar
